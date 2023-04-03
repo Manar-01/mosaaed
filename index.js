714626fc705ff06fb1982116679659981327be41
Manar-01 <s439000802@st.uqu.edu.sa>
2023-04-03 07:23:22 +0300

    mosaaed completed

--- index.js
+++ index.js
@@ -5,16 +5,42 @@ function filter(city) {
         if (!c.includes(city)) {
             cards[index].hidden = true;
         }
-        else{
-            cards[index].hidden = false; 
-        }   
+        else {
+            cards[index].hidden = false;
+        }
     }
-    document.getElementById("more").hidden=true; 
+    document.getElementById("more").hidden = true;
 }
-function more(){
+function more() {
     var cards = document.getElementsByClassName("col-card");
     for (let index = 0; index < cards.length; index++) {
-        cards[index].hidden = false;   
+        cards[index].hidden = false;
     }
-    document.getElementById("more").hidden=true; 
+    document.getElementById("more").hidden = true;
+}
+
+function flip(element,n){
+    var btn = document.getElementsByClassName("btn-2");
+    for (let index = 0; index < btn.length; index++) {
+        if (btn[index] === element) {
+            element.classList.add("active-1");
+        }
+        else{
+            btn[index].classList.remove("active-1");
+        }
+    }
+    var container = document.getElementsByClassName("container-1");
+    for (let index = 0; index < container.length; index++) {
+        if (index == n) {
+            container[index].hidden = false;
+        }
+        else{
+            container[index].hidden = true;
+        }
+    }
+}
+window.onload = function(){
+    if(window.location.hash=='#id02'){
+        document.getElementById('id02').style.display='block';
+    }
 }
